26e38690a80818f3a7af6f8b997e59a296dded8b
ngmattng <honsum.matt.ng@gmail.com>
2021-02-03 22:59:08 -0600

    updated dependency versions and added view binding

---
+++ app/src/main/java/com/servicenow/exercise_kotlin/ReviewListActivityNew.kt
@@ -0,0 +1,26 @@
+package com.servicenow.exercise_kotlin
+
+import android.app.Activity
+import android.os.Bundle
+import com.servicenow.exercise.databinding.ActivityReviewListBinding
+
+/**
+ * Created by Matt Ng on 2/3/21.
+ */
+class ReviewListActivityNew : Activity() {
+
+    private var _binding: ActivityReviewListBinding? = null
+    private val binding: ActivityReviewListBinding
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityReviewListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
